2ccbb2c922ab679d6426dfa0e1932cfe8cbb3287
ramiforde <ramiforde@gmail.com>
2020-01-15 19:16:11 -0500

    Changed around layout and made most of my html through javascript to make it easier to code the logic for moment.js

--- assets/javascript/calendar.js
+++ assets/javascript/calendar.js
@@ -1,18 +1,29 @@
-// Store value from element ID's and Classes
-var containerSlotsEl = $("#container-slots");
-var saveButtonEl = $(".save-button");
-var textArea = $(".todo-text");
+// catch all values needed
+// Time blocks
 
-// Create our elements for input and save buttons
-
-saveButtonEl.on("click", function(){
-    // we want to text put in text area to local storage when we click the save button
-   var save = $("this").attr("data-form");
-   console.log(this);
-   var event = $(this).parent().siblings("input").val()
-   localStorage.getItem("save", event);
+var currentDate = moment().format("dddd, MMMM Do");
+$("#currentDay").text(currentDate);
+var headDateDiv = $("<p>");
+headDateDiv.text(currentDate);
+headDateDiv.addClass("text-center");
 
 
 
-    
-})
+var timeSlots = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"];
+
+for(var i = 0; i < timeSlots.length; i++){
+    var timeSlotDiv = $("<div>").addClass("time-slot-style row");
+    $("#time-slots").append(timeSlotDiv);
+    // Create row for each div
+    var timeRow = $("<div>").addClass("col-md-2");
+    timeRow.text(timeSlots[i]);
+    timeSlotDiv.append(timeRow);
+    // Create textarea and append them to timeslotDiv
+    var textAreaInput = $("<textarea>").addClass("col-md-8 ");
+    timeSlotDiv.append(textAreaInput);
+    // Create buttons and append them to timeslotDiv
+    var saveInputArea = $("<button>").addClass("col-md-2 fa fa-save pt-4 btn btn-primary"); 
+    timeSlotDiv.append(saveInputArea);
+}
+
+
